f1bc081fe0198bbc111da93469053c7441d5b244
virtualabishek <717abishekneupane@gmail.com>
2024-08-29 17:46:43 +0545

    nepaliHealthCare

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import WelcomePage from "./components/WelcomePage";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <WelcomePage />
+    </>
+  );
+}
+
+export default App;
