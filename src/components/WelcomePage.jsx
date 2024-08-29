b991b81890e8624baa31cdc12307723788c9bf64
virtualabishek <717abishekneupane@gmail.com>
2024-08-29 17:54:06 +0545

    done.

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -4,13 +4,6 @@ import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-const navigation = [
-  { name: "Product", href: "#" },
-  { name: "Features", href: "#" },
-  { name: "Marketplace", href: "#" },
-  { name: "Company", href: "#" },
-];
-
 export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
