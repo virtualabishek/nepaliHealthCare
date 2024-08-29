fe5b56f324bf3583c29baba791afd7b4340361f9
virtualabishek <717abishekneupane@gmail.com>
2024-08-29 17:53:37 +0545

    done for today

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -41,12 +41,11 @@ export default function Example() {
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Data to enrich your online business
+              Your Health, Our Priority
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+              An app to take care of you. We provide reliable health insights,
+              expert guidance, and easy tools for your well-being.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
